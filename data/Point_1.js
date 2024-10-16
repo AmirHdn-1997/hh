ab2fb5226fca7fad67b1c5359dec44d35fcab8ff
AmirHdn-1997 <amir.hadian7676@gmail.com>
2024-10-16 08:51:57 +0330

    webgis

---
+++ data/Point_1.js
@@ -0,0 +1,8 @@
+var json_Point_1 = {
+"type": "FeatureCollection",
+"name": "Point_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "E": 637214.0, "N": 3301053.0 }, "geometry": { "type": "Point", "coordinates": [ 52.420222536561248, 29.832334593076066 ] } }
+]
+}
